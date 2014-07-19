ccab369a4024b21f10e7350d51daa6c9916f75d3
Kevin Ingersoll <kingersoll@gmail.com>
2014-07-18 18:59:59 -0700

    Restructure Lua script for a bit better readability

--- lua/redirect.lua
+++ lua/redirect.lua
@@ -1,20 +1,4 @@
 local resolver = require "resty.dns.resolver"
-local r, err = resolver:new { nameservers = { "8.8.8.8", "8.8.4.4" } }
-
-if not r then
-    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
-    return ngx.say("failed to instantiate the resolver:", err)
-end
-
--- local start = os.clock()
-local answers, err = r:query(ngx.var.host, { qtype = resolver.TYPE_TXT })
--- ngx.header["X-Query-Time"] = os.clock() - start
-
-if not answers then
-    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
-    return ngx.say("failed to query the DNS server:", err)
-end
-
 
 local codes = {
     ["301"] = 301,
@@ -23,45 +7,64 @@ local codes = {
     default = 302
 }
 
+function fail (...)
+    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+    ngx.say("<h1>Error</h1>")
+    ngx.say(...)
+end
+
+function fallback ()
+    ngx.redirect("http://redirect.name/")
+end
+
 -- escape patterns
 function escape (s)
     return string.gsub(s, "[%^%$%(%)%%%.%[%]%*%+%-%?]", "%%%0")
 end
 
 
-if not answers.errcode then
-    for i, ans in ipairs(answers) do
-        -- wrap in `repeat` so we can use `break` to mimic Lua's missing `continue`
-        repeat
-            if ans.type ~= resolver.TYPE_TXT then break end
+local r, err = resolver:new { nameservers = { "8.8.8.8", "8.8.4.4" } }
+if not r then return fail("Failed to instantiate the resolver: ", err) end
 
-            local m, err = ngx.re.match(ans.txt, "redirect\\.name=(?:(?<from>/\\S*)\\s+)?(?<to>(?:https?://\\S+|/\\S*))(?:\\s+(?<status>\\d+))?", "i")
-            if not m then break end
+local answers, err = r:query(ngx.var.host, { qtype = resolver.TYPE_TXT })
+if not answers then return fail("Failed to query the DNS server: ", err) end
 
-            local uri = ngx.var.request_uri
-            -- ngx.header["X-URI"] = uri
-            -- ngx.header["X-From"] = m.from
-            local from = escape(m.from or "/*")
-            -- ngx.header["X-From-Escaped"] = from
-            local pattern = "^" .. string.gsub(from, "%%%*", "(.*)", 1) .. "$"
-            -- ngx.header["X-Pattern"] = pattern
+-- fall back to redirect when query returns bad hostname, non-existent hostname, or other errors
+if answers.errcode then return fallback() end
 
-            if not string.find(uri, pattern) then break end
 
-            -- ngx.header["X-To"] = m.to
-            local to = escape(m.to)
-            -- ngx.header["X-To-Escaped"] = to
-            local replace = string.gsub(to, "%%%*", "%%1", 1)
-            -- ngx.header["X-Replace"] = replace
-            local target = string.gsub(uri, pattern, replace)
-            -- ngx.header["X-Target"] = target
+for i, ans in ipairs(answers) do
+    -- wrap in `repeat` so we can use `break` to mimic Lua's missing `continue`
+    repeat
+        if ans.type ~= resolver.TYPE_TXT then break end
 
-            local status = codes[m.status] or codes.default
+        local m, err = ngx.re.match(ans.txt, "redirect\\.name=(?:(?<from>/\\S*)\\s+)?(?<to>(?:https?://\\S+|/\\S*))(?:\\s+(?<status>\\d+))?", "i")
+        if not m then break end
 
-            return ngx.redirect(target, status)
-        until true
-    end
+        local uri = ngx.var.request_uri
+        -- ngx.header["X-URI"] = uri
+        -- ngx.header["X-From"] = m.from
+        local from = escape(m.from or "/*")
+        -- ngx.header["X-From-Escaped"] = from
+        local pattern = "^" .. string.gsub(from, "%%%*", "(.*)", 1) .. "$"
+        -- ngx.header["X-Pattern"] = pattern
+
+        if not string.find(uri, pattern) then break end
+
+        -- ngx.header["X-To"] = m.to
+        local to = escape(m.to)
+        -- ngx.header["X-To-Escaped"] = to
+        local replace = string.gsub(to, "%%%*", "%%1", 1)
+        -- ngx.header["X-Replace"] = replace
+        local target = string.gsub(uri, pattern, replace)
+        -- ngx.header["X-Target"] = target
+
+        local status = codes[m.status] or codes.default
+
+        return ngx.redirect(target, status)
+    until true
 end
 
+
 -- fall back to redirect.name for documentation
-return ngx.redirect("http://redirect.name/")
+return fallback()
