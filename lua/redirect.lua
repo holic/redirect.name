0aa9d0fe65c400bf48af62fd2d4ce3c27602a270
Kevin Ingersoll <kingersoll@gmail.com>
2014-07-18 19:15:31 -0700

    Only do pattern matching as necessary

--- lua/redirect.lua
+++ lua/redirect.lua
@@ -1,5 +1,6 @@
 local resolver = require "resty.dns.resolver"
 
+-- supported redirect status codes
 local codes = {
     ["301"] = 301,
     ["302"] = 302,
@@ -7,12 +8,14 @@ local codes = {
     default = 302
 }
 
+-- hard failures
 function fail (...)
     ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
     ngx.say("<h1>Error</h1>")
     ngx.say(...)
 end
 
+-- fall back to documentation site
 function fallback ()
     ngx.redirect("http://redirect.name/")
 end
@@ -29,7 +32,7 @@ if not r then return fail("Failed to instantiate the resolver: ", err) end
 local answers, err = r:query(ngx.var.host, { qtype = resolver.TYPE_TXT })
 if not answers then return fail("Failed to query the DNS server: ", err) end
 
--- fall back to redirect when query returns bad hostname, non-existent hostname, or other errors
+-- fall back when query returns bad hostname, non-existent hostname, or other errors
 if answers.errcode then return fallback() end
 
 
@@ -38,25 +41,32 @@ for i, ans in ipairs(answers) do
     repeat
         if ans.type ~= resolver.TYPE_TXT then break end
 
-        local m, err = ngx.re.match(ans.txt, "redirect\\.name=(?:(?<from>/\\S*)\\s+)?(?<to>(?:https?://\\S+|/\\S*))(?:\\s+(?<status>\\d+))?", "i")
+        local m, err = ngx.re.match(ans.txt, "redirect\\.name=(?:(?<from>/\\S*)\\s+)?(?<to>(?:https?://\\S+|/\\S*))(?:\\s+(?<status>301|302))?", "i")
         if not m then break end
 
-        local uri = ngx.var.request_uri
-        local from = escape(m.from or "/*")
-        local pattern = "^" .. string.gsub(from, "%%%*", "(.*)", 1) .. "$"
-
-        if not string.find(uri, pattern) then break end
-
-        local to = escape(m.to)
-        local replace = string.gsub(to, "%%%*", "%%1", 1)
-        local target = string.gsub(uri, pattern, replace)
-
+        local to = m.to
         local status = codes[m.status] or codes.default
+        -- no `from` assumes catch-all, so redirect immediately to `to`
+        if not m.from then return ngx.redirect(to, status) end
 
-        return ngx.redirect(target, status)
+        local uri = ngx.var.request_uri
+        local from, count = string.gsub(escape(m.from), "%%%*", "(.*)", 1)
+        from = "^" .. from .. "$"
+
+        -- if we can't find the pattern, break to continue to next record
+        if not string.find(uri, from) then break end
+
+        -- wildcard replacement of `uri` if there's a wildcard in our `from` path
+        if count then
+            to = escape(to)
+            to = string.gsub(to, "%%%*", "%%1", 1)
+            to = string.gsub(uri, from, to)
+        end
+
+        return ngx.redirect(to, status)
     until true
 end
 
 
--- fall back to redirect.name for documentation
+-- fall back if no records match
 return fallback()
