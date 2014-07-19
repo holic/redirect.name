08ef13d8222256bc99c97feee6f468f7a3a49a3b
Kevin Ingersoll <kingersoll@gmail.com>
2014-07-18 18:33:27 -0700

    Initial commit

---
+++ fabfile.py
@@ -0,0 +1,56 @@
+from fabric.api import *
+from fabric.contrib.files import *
+
+env.hosts = ['root@107.170.45.49']
+
+OPENRESTY = 'ngx_openresty-1.7.2.1'
+
+@task
+def install_packages():
+	sudo('apt-get update')
+	sudo('apt-get install -y libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make')
+
+@task
+def install_openresty():
+	if exists(OPENRESTY) is False:
+		run('wget -O - http://openresty.org/download/%s.tar.gz | tar xzf -' % (OPENRESTY))
+	with cd(OPENRESTY):
+		run('./configure')
+		run('make')
+		sudo('make install')
+
+@task
+def nginx(command):
+	sudo('service nginx %(command)s' % locals())
+
+@task
+def add_init_script():
+	put('nginx.init.d', '/etc/init.d/nginx')
+	sudo('chmod +x /etc/init.d/nginx')
+	sudo('update-rc.d -f nginx defaults')
+	nginx('start')
+
+@task
+def update_config():
+	with cd('/usr/local/openresty/nginx/conf'):
+		sudo('mv -n nginx.conf nginx.conf.default')
+		put('nginx.conf', 'nginx.conf')
+	nginx('reload')
+
+@task
+def update_scripts():
+	sudo('mkdir -p /usr/local/openresty/nginx/lua')
+	with cd('/usr/local/openresty/nginx/lua'):
+		put('redirect.lua', 'redirect.lua')
+	nginx('reload')
+
+@task
+def install():
+	install_packages()
+	install_openresty()
+
+@task
+def init():
+	add_init_script()
+	update_config()
+	update_scripts()
---
+++ redirect.lua
@@ -0,0 +1,65 @@
+local resolver = require "resty.dns.resolver"
+local r, err = resolver:new { nameservers = { "8.8.8.8", "8.8.4.4" } }
+
+if not r then
+    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+    return ngx.say("failed to instantiate the resolver:", err)
+end
+
+-- local start = os.clock()
+local answers, err = r:query(ngx.var.host, { qtype = resolver.TYPE_TXT })
+-- ngx.header["X-Query-Time"] = os.clock() - start
+
+if not answers then
+    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+    return ngx.say("failed to query the DNS server:", err)
+end
+
+
+local codes = {
+    ["301"] = 301,
+    ["302"] = 302,
+    -- ["307"] = 307, -- not supported by openresty
+    default = 302
+}
+
+-- escape patterns
+function escape (s)
+    return string.gsub(s, "[%^%$%(%)%%%.%[%]%*%+%-%?]", "%%%0")
+end
+
+
+if not answers.errcode then
+    for i, ans in ipairs(answers) do
+        if ans.type == resolver.TYPE_TXT then
+            local m, err = ngx.re.match(ans.txt, "redirect\\.name=(?:(?<from>/\\S*)\\s+)?(?<to>(?:https?://\\S+|/\\S*))(?:\\s+(?<status>\\d+))?", "i")
+
+            if m then
+                local uri = ngx.var.request_uri
+                -- ngx.header["X-URI"] = uri
+                -- ngx.header["X-From"] = m.from
+                local from = escape(m.from or "/*")
+                -- ngx.header["X-From-Escaped"] = from
+                local pattern = "^" .. string.gsub(from, "%%%*", "(.*)", 1) .. "$"
+                -- ngx.header["X-Pattern"] = pattern
+
+                if string.find(uri, pattern) then
+                    -- ngx.header["X-To"] = m.to
+                    local to = escape(m.to)
+                    -- ngx.header["X-To-Escaped"] = to
+                    local replace = string.gsub(to, "%%%*", "%%1", 1)
+                    -- ngx.header["X-Replace"] = replace
+                    local target = string.gsub(uri, pattern, replace)
+                    -- ngx.header["X-Target"] = target
+
+                    local status = codes[m.status] or codes.default
+
+                    return ngx.redirect(target, status)
+                end
+            end
+        end
+    end
+end
+
+-- fall back to redirect.name for documentation
+return ngx.redirect("http://redirect.name/")
