a2baba25c30079807e9624242aa8d14c782f0a53
Kevin Ingersoll <kingersoll@gmail.com>
2014-07-19 11:48:58 -0700

    Always fall back to documentation site on errors

--- lua/redirect.lua
+++ lua/redirect.lua
@@ -1,12 +1,3 @@
-local resolver = require "resty.dns.resolver"
-
--- hard failures
-local function fail (...)
-    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
-    ngx.say("<h1>Error</h1>")
-    ngx.say(...)
-end
-
 -- fall back to documentation site
 local function fallback (reason)
     local target = "http://redirect.name/"
@@ -15,14 +6,17 @@ local function fallback (reason)
 end
 
 
+local resolver = require "resty.dns.resolver"
+-- instantiate DNS resolver
 local r, err = resolver:new { nameservers = { "8.8.8.8", "8.8.4.4" } }
-if not r then return fail("Failed to instantiate the resolver: ", err) end
+if not r then return fallback("Failed to instantiate the resolver: " .. err) end
 
+-- query hostname for TXT records
 local answers, err = r:query(ngx.var.host, { qtype = resolver.TYPE_TXT })
-if not answers then return fail("Failed to query the DNS server: ", err) end
+if not answers then return fallback("Failed to query the DNS server: " .. err) end
 
 -- fall back when query returns bad hostname, non-existent hostname, or other errors
-if answers.errcode then return fallback(answers.errcode .. " " .. answers.errstr) end
+if answers.errcode then return fallback("Could not resolve hostname: " .. answers.errcode .. " " .. answers.errstr) end
 
 
 local parse_redirect = require "parse_redirect"
@@ -38,6 +32,5 @@ for i, ans in ipairs(answers) do
     end
 end
 
-
 -- fall back if no records match
-return fallback("No paths matched.")
+return fallback("No paths matched")
