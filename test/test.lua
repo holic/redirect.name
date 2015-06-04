52f79c2dce41a541a373c088a20a591c3e7b2a9d
Kevin Ingersoll <kingersoll@gmail.com>
2015-06-03 22:04:39 -0700

    Remove old code and scripts

--- test/test.lua
+++
@@ -1,7 +0,0 @@
--- add path so we can require from lua directory
-package.path = "../lua/?.lua;" .. package.path
-
-require "redirect_parse"
-require "redirect_translate"
-
-print("All tests passed!")
