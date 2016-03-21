3669ad895dfdb910e868ea21e2071e2ff727237f
Ken Gorab <ken.gorab@gmail.com>
2016-03-20 21:41:10 -0400

    Redirect an incoming POST request and body, using a 307 status code

--- translate_test.go
+++ translate_test.go
@@ -5,25 +5,25 @@ import "testing"
 func TestTranslate(t *testing.T) {
 	var redirect *Redirect
 
-	redirect = Translate("/", nil)
+	redirect = Translate("GET", "/", nil)
 	if redirect != nil {
 		t.Errorf("Expected %#v to be %#v", redirect, nil)
 	}
 
-	redirect = Translate("/", &Config{To: "https://example.com/"})
+	redirect = Translate("GET", "/", &Config{To: "https://example.com/"})
 	assertEqual(t, redirect.Location, "https://example.com/")
 	assertEqual(t, redirect.Status, 302)
 
-	redirect = Translate("/", &Config{To: "https://example.com/", RedirectState: "301"})
+	redirect = Translate("GET", "/", &Config{To: "https://example.com/", RedirectState: "301"})
 	assertEqual(t, redirect.Location, "https://example.com/")
 	assertEqual(t, redirect.Status, 301)
 
-	redirect = Translate("/", &Config{From: "/twitter", To: "https://example.com/", RedirectState: "permanently"})
+	redirect = Translate("GET", "/", &Config{From: "/twitter", To: "https://example.com/", RedirectState: "permanently"})
 	if redirect != nil {
 		t.Errorf("Expected %#v to be %#v", redirect, nil)
 	}
 
-	redirect = Translate("/", &Config{From: "/", To: "https://example.com/", RedirectState: "permanently"})
+	redirect = Translate("GET", "/", &Config{From: "/", To: "https://example.com/", RedirectState: "permanently"})
 	assertEqual(t, redirect.Location, "https://example.com/")
 	assertEqual(t, redirect.Status, 301)
 }
@@ -31,32 +31,45 @@ func TestTranslate(t *testing.T) {
 func TestTranslateWildcard(t *testing.T) {
 	var redirect *Redirect
 
-	redirect = Translate("/about-us", &Config{From: "/*", To: "http://example.com/"})
+	redirect = Translate("GET", "/about-us", &Config{From: "/*", To: "http://example.com/"})
 	assertEqual(t, redirect.Location, "http://example.com/")
 	assertEqual(t, redirect.Status, 302)
 
-	redirect = Translate("/about-us", &Config{From: "/*", To: "http://example.com/*"})
+	redirect = Translate("GET", "/about-us", &Config{From: "/*", To: "http://example.com/*"})
 	assertEqual(t, redirect.Location, "http://example.com/about-us")
 	assertEqual(t, redirect.Status, 302)
 
-	redirect = Translate("/about-us", &Config{From: "/*", To: "http://example.com/*"})
+	redirect = Translate("GET", "/about-us", &Config{From: "/*", To: "http://example.com/*"})
 	assertEqual(t, redirect.Location, "http://example.com/about-us")
 	assertEqual(t, redirect.Status, 302)
 
-	redirect = Translate("/blog/1", &Config{From: "/*/1", To: "http://example.com/*", RedirectState: "temporarily"})
+	redirect = Translate("GET", "/blog/1", &Config{From: "/*/1", To: "http://example.com/*", RedirectState: "temporarily"})
 	assertEqual(t, redirect.Location, "http://example.com/blog")
 	assertEqual(t, redirect.Status, 302)
 
-	redirect = Translate("/wildcard", &Config{From: "/*", To: "http://example.com/**"})
+	redirect = Translate("GET", "/wildcard", &Config{From: "/*", To: "http://example.com/**"})
 	assertEqual(t, redirect.Location, "http://example.com/wildcard*")
 	assertEqual(t, redirect.Status, 302)
 
-	redirect = Translate("/wildcard", &Config{From: "/**", To: "http://example.com/*"})
+	redirect = Translate("GET", "/wildcard", &Config{From: "/**", To: "http://example.com/*"})
 	if redirect != nil {
 		t.Errorf("Expected %#v to be %#v", redirect, nil)
 	}
 
-	redirect = Translate("/wildcard*", &Config{From: "/**", To: "http://example.com/*"})
+	redirect = Translate("GET", "/wildcard*", &Config{From: "/**", To: "http://example.com/*"})
 	assertEqual(t, redirect.Location, "http://example.com/wildcard")
 	assertEqual(t, redirect.Status, 302)
+}
+
+func TestTranslateStatus(t *testing.T)  {
+	var redirect *Redirect
+
+	redirect = Translate("POST", "/", &Config{From: "/*", To: "http://example.com/*"})
+	assertEqual(t, redirect.Status, 307)
+
+	redirect = Translate("POST", "/", &Config{From: "/*", To: "http://example.com/*", RedirectState: "permanently"})
+	assertEqual(t, redirect.Status, 307)
+
+	redirect = Translate("POST", "/", &Config{From: "/*", To: "http://example.com/*", RedirectState: "temporarily"})
+	assertEqual(t, redirect.Status, 307)
 }
