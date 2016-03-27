c9e488a433d2c722ff8d6b1217325a4dc5fe7dd1
Matt Klich <matt.klich@wayin.com>
2016-03-27 01:13:16 -0600

    Catch all redirects need to come last.
    If catch all redirects are allowed to run in the same stage as regular
    redirects then you get inconsistant redirects because of the round robin
    nature of the record matching.
    
    I've also enabled some debug logging if you set an environment varibale
    'DEBUG' to a non-empty value.

--- server.go
+++ server.go
@@ -11,6 +11,17 @@ import (
 	"time"
 )
 
+var debug = os.Getenv("DEBUG")
+func logDebug(format string, args ...interface{}) {
+	if debug != "" {
+		if len(args) == 0 {
+			log.Print(format)
+		} else {
+			log.Printf(format, args)
+		}
+	}
+}
+
 func fallback(w http.ResponseWriter, r *http.Request, reason string) {
 	location := "http://redirect.name/"
 	if reason != "" {
@@ -30,13 +41,39 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	logDebug("Checking URL: %s", r.URL.String())
+
+	var catch_alls []string
+	logDebug("Begin record search.")
 	for _, record := range txt {
+		logDebug("Inspecting: %s", record)
+		config := Parse(record)
+		if strings.TrimSpace(config.From) == "" {
+			logDebug("Saving catch all.")
+			catch_alls = append(catch_alls, record)
+			_ = catch_alls
+			continue
+		}
 		redirect := Translate(r.URL.String(), Parse(record))
 		if redirect != nil {
 			http.Redirect(w, r, redirect.Location, redirect.Status)
+			logDebug("Found matching record.")
 			return
 		}
 	}
+	logDebug("End record search")
+
+	logDebug("Begin catch alls")
+	for _, record := range catch_alls {
+		logDebug("Inspecting: %s", record)
+		redirect := Translate(r.URL.String(), Parse(record))
+		if redirect != nil {
+			http.Redirect(w, r, redirect.Location, redirect.Status)
+			logDebug("Found catch all.")
+			return
+		}
+	}
+	logDebug("No match found.")
 
 	fallback(w, r, "No paths matched")
 }
